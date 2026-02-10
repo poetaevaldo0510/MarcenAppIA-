3b079b01e4d49981bb0a0ce6e55d8eb2db9f3ac5
poetaevaldo0510 <evaldo0510@gmail.com>
2026-02-10 10:22:38 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // <- ESTA LINHA É O PROBLEMA
+import App from "./App"; // <- aqui está o problema
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
